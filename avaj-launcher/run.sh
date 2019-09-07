86d123867c0bfea599b879c9e6587822b52b98ac
Mariia Pashynska <42279868+mashenkaPas@users.noreply.github.com>
2019-09-07 15:17:55 -0700

    Update run.sh

--- avaj-launcher/run.sh
+++ avaj-launcher/run.sh
@@ -1,2 +1,1 @@
-//java com.company.simulator.Simulator "/Users/richardcarlton/scenario.txt" 
-java -cp src com.company.simulator.Simulator  "/Users/richardcarlton/scenario.txt"
+java -cp src com.company.simulator.Simulator  "/path/to/folder/scenario.txt"
